ffc080f4180d3f20d7eec1a9c3f3f93f1bc0633c
Matias Casco <kon-dcasco@bancobasa.com.py>
2025-05-17 19:24:21 -0300

    feat: add dotenv and scripts

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,7 +16,7 @@ const publicPath = path.resolve(__dirname, "public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT, (err) => {
   if (err) throw new Error(err);
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port " + process.env.PORT);
 });
